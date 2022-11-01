f2021469f1e00918751ba3d81ddfe0d6fdfc5fb0
Chris Bongers <chrisbongers@gmail.com>
2022-11-01 07:31:38 +0200

    feat: testing next 13 layouts

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+export default function DashboardLayout({children}: {
+    children: React.ReactNode
+}) {
+    return <main className='flex'>
+        <aside className='w-64 bg-cyan-100 rounded-xl m-4'>
+            <nav>
+                <ul className='gap-4 flex flex-col p-4'>
+                    <li><a href='#'>Account</a></li>
+                    <li><a href='#'>Settings</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <main className='p-4'>{children}</main>
+    </main>;
+}
